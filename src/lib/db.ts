bdd30bd36c09aae6bbbffe867b574306b3597f08
D3eviee <80790777+D3eviee@users.noreply.github.com>
2025-05-16 02:42:20 +0200

    xd prisma

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/index"
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 const prisma = globalForPrisma.prisma || new PrismaClient()
